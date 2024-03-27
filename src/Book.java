ec57befdc45d7edc3ac13e77c90ed62a3f2e53ab
JCassarino <jmerino81@gmail.com>
2024-03-27 18:31:08 -0400

    - Book data filled out - Book class and object made - Allows user to choose a genre of book to look at - Reruns loop if user enters invalid choice for genre

--- src/Book.java
+++ src/Book.java
@@ -1,19 +1,20 @@
-public class BookShelf {
-private static class Book{
+
+class Book {
     String title;
     String author;
     String pubDate;
     double price;
     int pageNum;
     int genreIndex;
-    public Book(String title, String author, String pubDate, double price, int pageNum, int genreIndex){
+
+    public Book(String title, String author, String pubDate, double price, int pageNum, int genreIndex) {
         this.title = title;
         this.author = author;
         this.pubDate = pubDate;
         this.price = price;
         this.pageNum = pageNum;
         this.genreIndex = genreIndex;
-        }
     }
 
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-            /*
-    1. History (Nonfiction) - American Prometheus, The Guns of August, Orientalism, 1776, An Army at Dawn
-    2. Historical Fiction - The Book Thief, A Tale of Two Cities, Shōgun, All Quiet On The Western Front, War and Peace
-    3. Science Fiction - The Three-Body Problem, Ender's Game, Dune, Foundation, I Robot
-    4. Mystery - The Body, Murder on the Orient Express, A Study in Scarlet, Where the Crawdads Sing, And Then There Were None
-    5. Fantasy - Harry Potter, Percy Jackson, Lord of the Rings, Game of Thrones, The Chronicles of Narnia
-     */
 
-    // Defining book data
-    String[] title = {
+// Populating arrays with book data to be used later
+    String[] titles = {
             "American Prometheus","The Guns of August","Orientalism","1776","An Army at Dawn",
             "The Book Thief","A Tale of Two Cities","Shōgun","All Quiet on the Western Front","War and Peace",
             "The Three-Body Problem","Ender's Game","Dune","Foundation","I, Robot",
@@ -17,7 +12,7 @@ public class Main {
             "Harry Potter and the Sorcerer's Stone","The Lightning Thief","The Fellowship of the Ring","A Game of Thrones","The lion, the Witch, and the Wardrobe",
     };
 
-    String[] author = {
+    String[] authors = {
             "Kai Bird, Martin J. Sherwin","Barbara W. Tuchman","Edward Said","David McCullough","Rick Atkinson",
             "Markus Zusak","Charles Dickens","James Clavell","Erich Maria Remarque","Leo Tolstoy",
             "Cixin Liu","Orson Scott Card","Frank Herbert","Isaac Asimov","Isaac Asimov",
@@ -25,7 +20,7 @@ public class Main {
             "J.K. Rowling","Rick Riordan","J.R.R. Tolkien","George R.R. Martin","C.S. Lewis"
     };
 
-    String[] pubDate = {
+    String[] pubDates = {
             "04/05/2005","1962","1978","05/24/2005","08/2002",
             "2005","11/26/1859","1975","1928","1867",
             "2008","01/1985","08/1965","1951","12/02/1950",
@@ -33,7 +28,7 @@ public class Main {
             "06/26/1997","06/01/2005","06/29/1954","08/01/1996","10/16/1950"
     };
 
-    double[] price = {
+    double[] prices = {
             15.15, 8.99, 12.99, 11.51, 14.69,
             7.83, 5.49, 22.49, 6.99, 12.49,
             19.59, 15.15, 20.99, 18.80, 13.56,
@@ -43,7 +38,7 @@ public class Main {
 
     };
 
-    int[] pageNum = {
+    int[] pageNums = {
             721, 551, 368, 386, 704,
             584, 408, 1299, 160, 1225,
             302, 324, 896, 255, 253,
@@ -52,7 +47,7 @@ public class Main {
 
     };
 
-    int[] genreIndex = {
+    int[] genreIndices = {
             1,1,1,1,1,
             2,2,2,2,2,
             3,3,3,3,3,
@@ -60,6 +55,61 @@ public class Main {
             5,5,5,5,5
     };
 
+    String[] genres = {
+            "History (Nonfiction)",
+            "Historical Fiction",
+            "Science Fiction",
+            "Mystery",
+            "Fantasy",
+    };
+
+    // Creating an array of "book" objects and populating it with the data listed above
+Book[] books = new Book[titles.length];
+for (int i = 0; i < books.length; i++){
+books[i] = new Book(
+        titles[i],
+        authors[i],
+        pubDates[i],
+        prices[i],
+        pageNums[i],
+        genreIndices[i]);
+}
+    // Declaring scanner, allows us to prompt user to choose their genre
+        Scanner in = new Scanner(System.in);
+
+    // Variable to store the selected genre, initialized as null to allow for declaration inside loop, which would then break the loop
+        String selectedGenre = null;
+
+    // Printing welcome text
+        System.out.println("Welcome to the IS147 Library Directory!");
+        System.out.println();
+
+while (selectedGenre == null) {
+    System.out.println("Please select the genre of book you would like to look at:");
+    for (int i = 0; i < genres.length; i++){
+        System.out.println("["+ (i+1) +"] - " + genres[i]);
+    }
+    int genreChoice = in.nextInt();
+
+
+    if (genreChoice >= 1 && genreChoice <= genres.length){
+        selectedGenre = genres[genreChoice-1];} // Valid choice, declares selectedGenre as a valid int, breaking the loop to continue the program
+
+    else {System.out.println("Invalid choice. Please try again.");} // Invalid choice, runs loop again
+}
+
+            System.out.println("Books in the " +selectedGenre+ " genre: ");
+            for(Book book : books){
+                if(genres[book.genreIndex].equals(selectedGenre)){
+                    System.out.println("Title: " + book.title);
+                    System.out.println("Author: " + book.author);
+                    System.out.println("Date Published: " + book.pubDate);
+                    System.out.println("Price: " + book.price);
+                    System.out.println("Page Count: " + book.pageNum);
+                    System.out.println();
+
+                }
+            }
 
     }
 }
