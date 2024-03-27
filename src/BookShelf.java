2055fc1c476b7c462c4dc456fc875c432f7ea249
JCassarino <jmerino81@gmail.com>
2024-03-27 17:20:25 -0400

    Book data entered

--- src/BookShelf.java
+++ src/BookShelf.java
@@ -1,22 +1,17 @@
 public class BookShelf {
-    /*
-    1. History (Nonfiction) - American Prometheus, The Guns of August, Orientalism, 1776, An Army At Dawn
-    2. Historical Fiction - The Book Thief, A Tale of Two Cities, Shogun, All Quiet On The Western Front, War and Peace
-    3. Science Fiction - The Three-Body Problem, Ender's Game, Dune, Foundation, I Robot
-    4. Mystery - The Body, Murder on the Orient Express, A Study in Scarlet, Where the Crawdads Sing, And Then There Were None
-    5. Fantasy - Harry Potter, Percy Jackson, Lord of the Rings, Game of Thrones, The Chronicles of Narnia
-     */
 private static class Book{
     String title;
     String author;
     String pubDate;
     double price;
+    int pageNum;
     int genreIndex;
-    public Book(String title, String author, String pubDate, double price, int genreIndex){
+    public Book(String title, String author, String pubDate, double price, int pageNum, int genreIndex){
         this.title = title;
         this.author = author;
-        this.price = price;
         this.pubDate = pubDate;
+        this.price = price;
+        this.pageNum = pageNum;
         this.genreIndex = genreIndex;
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,64 @@
 public class Main {
     public static void main(String[] args) {
+            /*
+    1. History (Nonfiction) - American Prometheus, The Guns of August, Orientalism, 1776, An Army at Dawn
+    2. Historical Fiction - The Book Thief, A Tale of Two Cities, Shōgun, All Quiet On The Western Front, War and Peace
+    3. Science Fiction - The Three-Body Problem, Ender's Game, Dune, Foundation, I Robot
+    4. Mystery - The Body, Murder on the Orient Express, A Study in Scarlet, Where the Crawdads Sing, And Then There Were None
+    5. Fantasy - Harry Potter, Percy Jackson, Lord of the Rings, Game of Thrones, The Chronicles of Narnia
+     */
 
     // Defining book data
+    String[] title = {
+            "American Prometheus","The Guns of August","Orientalism","1776","An Army at Dawn",
+            "The Book Thief","A Tale of Two Cities","Shōgun","All Quiet on the Western Front","War and Peace",
+            "The Three-Body Problem","Ender's Game","Dune","Foundation","I, Robot",
+            "The Body","Murder on the Orient Express","A Study in Scarlet","Where the Crawdads Sing","And Then There Were None",
+            "Harry Potter and the Sorcerer's Stone","The Lightning Thief","The Fellowship of the Ring","A Game of Thrones","The lion, the Witch, and the Wardrobe",
+    };
+
+    String[] author = {
+            "Kai Bird, Martin J. Sherwin","Barbara W. Tuchman","Edward Said","David McCullough","Rick Atkinson",
+            "Markus Zusak","Charles Dickens","James Clavell","Erich Maria Remarque","Leo Tolstoy",
+            "Cixin Liu","Orson Scott Card","Frank Herbert","Isaac Asimov","Isaac Asimov",
+            "Stephen King","Agatha Christie","Arthur Conan Doyle","Delia Owens","Agatha Christie",
+            "J.K. Rowling","Rick Riordan","J.R.R. Tolkien","George R.R. Martin","C.S. Lewis"
+    };
+
+    String[] pubDate = {
+            "04/05/2005","1962","1978","05/24/2005","08/2002",
+            "2005","11/26/1859","1975","1928","1867",
+            "2008","01/1985","08/1965","1951","12/02/1950",
+            "1982","01/01/1934","1887","08/14/2018","11/06/1939",
+            "06/26/1997","06/01/2005","06/29/1954","08/01/1996","10/16/1950"
+    };
+
+    double[] price = {
+            15.15, 8.99, 12.99, 11.51, 14.69,
+            7.83, 5.49, 22.49, 6.99, 12.49,
+            19.59, 15.15, 20.99, 18.80, 13.56,
+            10.48, 8.99, 8.88, 10.94, 16.55,
+            8.64, 5.99, 21.00, 11.00, 7.64
+
+
+    };
+
+    int[] pageNum = {
+            721, 551, 368, 386, 704,
+            584, 408, 1299, 160, 1225,
+            302, 324, 896, 255, 253,
+            192, 256, 192, 368, 272,
+            223, 377, 423, 694, 172
+
+    };
+
+    int[] genreIndex = {
+            1,1,1,1,1,
+            2,2,2,2,2,
+            3,3,3,3,3,
+            4,4,4,4,4,
+            5,5,5,5,5
+    };
 
 
     }
