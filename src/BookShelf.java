6b40d8473d6a2e8ad50c9c2a72832d9b9a30dbea
JCassarino <jmerino81@gmail.com>
2024-03-27 15:28:00 -0400

    Bookshelf class

---
+++ src/BookShelf.java
@@ -0,0 +1,10 @@
+public class BookShelf {
+    /*
+    1. History (Nonfiction) - American Prometheus, The Guns of August, Orientalism, 1776, An Army At Dawn
+    2. Historical Fiction - The Book Thief, A Tale of Two Cities, Shogun, All Quiet On The Western Front, War and Peace
+    3. Science Fiction - 3 Body Problem, Ender's Game, Dune, Foundation, I Robot
+    4. Mystery - The Body, Murder on the Orient Express, A Study in Scarlet, Where the Crawdads Sing, And Then There Were None
+    5. Fantasy - Harry Potter, Percy Jackson, Lord of the Rings, Game of Thrones, The Chronicles of Narnia
+     */
+
+}
