d3400704d39f9597e87b259bbffb72302f5dda81
khadijaharafat <karafat1@umbc.edu>
2024-04-16 09:37:59 -0400

    Added switch statements (lines 100-142)

--- src/Main.java
+++ src/Main.java
@@ -97,7 +97,47 @@ public class Main {
         }
 
     }
+    public static void displayMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
 
+        do {
+            System.out.println("\n===== Library Menu =====");
+            System.out.println("1. Add a book");
+            System.out.println("2. Remove a book");
+            System.out.println("3. Search for a book");
+            System.out.println("4. Display all books");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
 
+            switch (choice) {
+                case 1:
+                    System.out.println("You chose to add a book.");
+                    // Call a method to add a book
+                    break;
+                case 2:
+                    System.out.println("You chose to remove a book.");
+                    // Call a method to remove a book
+                    break;
+                case 3:
+                    System.out.println("You chose to search for a book.");
+                    // Call a method to search for a book
+                    break;
+                case 4:
+                    System.out.println("You chose to display all books.");
+                    // Call a method to display all books
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 5);
+
+        scanner.close();
+    }
 }
+
 
